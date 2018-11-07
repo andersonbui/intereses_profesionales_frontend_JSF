8bbf9f16bb3223a0ad173fcca96993f90fcbdfb7
anderson <abuitron@bellvoz.com>
2018-11-06 22:38:33 -0500

    interfaz login adaptada sin funcionalidad completa

--- src/main/java/com/ingesoft/interpro/controladores/LoginController.java
+++ src/main/java/com/ingesoft/interpro/controladores/LoginController.java
@@ -5,14 +5,20 @@
  */
 package com.ingesoft.interpro.controladores;
 
+import com.ingesoft.interpro.entidades.Usuario;
+import com.ingesoft.interpro.facades.UsuarioFacade;
 import java.io.IOException;
 import java.io.Serializable;
+import java.security.Principal;
 import java.util.Map;
 import java.util.Properties;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import org.brickred.socialauth.AuthProvider;
 import org.brickred.socialauth.Profile;
@@ -30,13 +36,25 @@ public class LoginController implements Serializable {
 
     private static final long serialVersionUID = 3658300628580536494L;
 
+    @EJB
+    UsuarioFacade ejbFacade;
+
+    private Usuario actual;
+
     private SocialAuthManager socialManager;
     private Profile profile;
+    String usuario;
+    String password;
+    boolean logueado;
 
     private final String mainURL = "http://localhost:8080/login_facebook/faces/index.xhtml";
     private final String redirectURL = "http://localhost:8080/login_facebook/faces/redirectHome.xhtml";
     //private final String redirectURL = "http://www.codewebpro.com/blog";
     private final String provider = "facebook";
+
+    public LoginController() {
+        logueado = false;
+    }
 
     public void conectar() {
         Properties prop = System.getProperties();
@@ -79,9 +97,86 @@ public class LoginController implements Serializable {
         this.profile = profile;
     }
 
-    public void salir() throws IOException {
-        setProfile(null);
-        FacesContext.getCurrentInstance().getExternalContext().redirect(mainURL);
+    public String getUsuario() {
+        return usuario;
     }
 
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void login() throws IOException {
+//        FacesContext context = FacesContext.getCurrentInstance();
+//        HttpServletRequest req = (HttpServletRequest) context.getExternalContext().getRequest();
+//        FacesMessage msg;
+//        if (req.getUserPrincipal() == null) {
+//            System.out.println("LOGUEO ACTIVADO----");
+//            try {
+//                req.login(this.usuario, this.password);
+//                msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenid@", this.usuario);
+//                logueado = true;
+//            } catch (ServletException e) {
+//                msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Usuario o contraseña incorrectos.");
+//                logueado = false;
+//                context.addMessage(null, msg);
+//                return;
+//            } catch (Exception e) {
+//                msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Error desconcido.");
+//                logueado = false;
+//                context.addMessage(null, msg);
+//                return;
+//            }
+//            Principal principal = req.getUserPrincipal();
+//            actual = ejbFacade.buscarPorUsuario(principal.getName());
+//            ExternalContext external = FacesContext.getCurrentInstance().getExternalContext();
+//            Map<String, Object> sessionMap = external.getSessionMap();
+//            sessionMap.put("user", usuario);
+//            context.addMessage(null, msg);
+//            System.out.println("sesion iniciada; logueado: " + logueado);
+//
+//        } else {
+//            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenid@", this.usuario);
+//            context.addMessage(null, msg);
+//            logueado = true;
+//            String nombreUsuario = req.getUserPrincipal().getName();
+//            actual = ejbFacade.buscarPorUsuario(nombreUsuario);
+//            System.out.println("sesion ya iniciada; logueado: " + logueado);
+//        }
+//        gruposUsuarios = actual.getGrupousuarioList();
+//        System.out.println("---" + actual.getNombres() + " " + actual.getApellidos());
+//        for (Grupousuario gruposUsuario : gruposUsuarios) {
+//            System.out.println("grupo: " + gruposUsuario);
+//        }
+        FacesContext.getCurrentInstance().getExternalContext().redirect("/intereses_profesionales/faces/vistas/pregunta/List.xhtml");
+    }
+
+    public String salir() throws IOException {
+
+        FacesContext fc = FacesContext.getCurrentInstance();
+        HttpServletRequest req = (HttpServletRequest) fc.getExternalContext().getRequest();
+        logueado = false;
+        try {
+            req.logout();
+            req.getSession().invalidate();
+            fc.getExternalContext().invalidateSession();
+
+        } catch (ServletException e) {
+            fc.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "FAILED", "Cerrar Sesion"));
+        }
+        FacesContext.getCurrentInstance().getExternalContext().redirect("/intereses_profesionales/faces/login.xhtml");
+//        return "/login.xhtml";
+        return "";
+    }
+
+    public boolean logueado() {
+        return logueado;
+    }
 }

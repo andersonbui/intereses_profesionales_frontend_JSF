5b997ad3df7b290a177fbd139e9d1c60d3e2ac01
anderson <abuitron@bellvoz.com>
2018-11-06 22:12:28 -0500

    adicion de login

---
+++ src/main/java/com/ingesoft/interpro/controladores/LoginController.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ingesoft.interpro.controladores;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Properties;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import org.brickred.socialauth.AuthProvider;
+import org.brickred.socialauth.Profile;
+import org.brickred.socialauth.SocialAuthConfig;
+import org.brickred.socialauth.SocialAuthManager;
+import org.brickred.socialauth.util.SocialAuthUtil;
+
+/**
+ *
+ * @author debian
+ */
+@SessionScoped
+@ManagedBean(name = "loginController")
+public class LoginController implements Serializable {
+
+    private static final long serialVersionUID = 3658300628580536494L;
+
+    private SocialAuthManager socialManager;
+    private Profile profile;
+
+    private final String mainURL = "http://localhost:8080/login_facebook/faces/index.xhtml";
+    private final String redirectURL = "http://localhost:8080/login_facebook/faces/redirectHome.xhtml";
+    //private final String redirectURL = "http://www.codewebpro.com/blog";
+    private final String provider = "facebook";
+
+    public void conectar() {
+        Properties prop = System.getProperties();
+        prop.put("graph.facebook.com.consumer_key", "329124954489538");
+        prop.put("graph.facebook.com.consumer_secret", "4c5e659fd3792cd6acd10e07e67a1855");
+        prop.put("graph.facebook.com.custom_permissions", "public_profile,email");
+
+        SocialAuthConfig socialConfig = SocialAuthConfig.getDefault();
+        try {
+            socialConfig.load(prop);
+            socialManager = new SocialAuthManager();
+            socialManager.setSocialAuthConfig(socialConfig);
+            String URLRetorno = socialManager.getAuthenticationUrl(provider, redirectURL);
+            FacesContext.getCurrentInstance().getExternalContext().redirect(URLRetorno);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getPerfilUsuario() throws Exception {
+        ExternalContext ex = FacesContext.getCurrentInstance().getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) ex.getRequest();
+
+        Map<String, String> parametros = SocialAuthUtil.getRequestParametersMap(request);
+
+        if (socialManager != null) {
+            AuthProvider provider = socialManager.connect(parametros);
+            this.setProfile(provider.getUserProfile());
+
+        }
+
+        FacesContext.getCurrentInstance().getExternalContext().redirect(mainURL);
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public void salir() throws IOException {
+        setProfile(null);
+        FacesContext.getCurrentInstance().getExternalContext().redirect(mainURL);
+    }
+
+}

dbd941b28045de37ba1885fc61c0ef12b9fc61c7
andersonbui <andersonbuitron@unicauca.edu.co>
2018-11-28 09:02:07 -0500

    calculo de  estadística de preguntas personalidad

---
+++ src/main/java/com/ingesoft/interpro/facades/RespuestaPorPersonalidadFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ingesoft.interpro.facades;
+
+import com.ingesoft.interpro.entidades.RespuestaPorPersonalidad;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author debian
+ */
+@Stateless
+public class RespuestaPorPersonalidadFacade extends AbstractFacade<RespuestaPorPersonalidad> {
+
+    @PersistenceContext(unitName = "com.ingeniosoft_intereses_profesionales_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public RespuestaPorPersonalidadFacade() {
+        super(RespuestaPorPersonalidad.class);
+    }
+    
+}

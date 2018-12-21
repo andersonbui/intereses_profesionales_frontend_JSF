65301348972aaedc223d9ff18f1c95245cd42697
Juan Manuel Díaz <juandiaz201093@gmail.com>
2018-12-20 23:29:12 -0500

    fujo de imagenes en evaluacion de ambiente imagenes - 75%

---
+++ src/main/java/com/ingesoft/interpro/controladores/util/Variables.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ingesoft.interpro.controladores.util;
+
+/**
+ *
+ * @author Juan Diaz PC
+ */
+public class Variables {
+
+    public static String ubicacionImagenes = "img/ambiente/";
+}

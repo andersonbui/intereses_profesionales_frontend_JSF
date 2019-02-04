2353543c17e13f03e9fda9dc150d165849999d2d
anderson <andersonbuitron@unicauca.edu.co>
2019-02-03 21:50:32 -0500

    pinta grafico de pruebe en estadisticas

--- src/main/java/com/ingesoft/interpro/controladores/EstadisticaController.java
+++ src/main/java/com/ingesoft/interpro/controladores/EstadisticaController.java
@@ -1,6 +1,9 @@
 package com.ingesoft.interpro.controladores;
 
+import be.ceau.chart.BarChart;
 import be.ceau.chart.color.Color;
+import be.ceau.chart.data.BarData;
+import be.ceau.chart.dataset.BarDataset;
 import com.ingesoft.interpro.controladores.util.Utilidades;
 import com.ingesoft.interpro.entidades.Encuesta;
 import com.ingesoft.interpro.entidades.EstudianteGrado;
@@ -19,8 +22,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import org.primefaces.event.SelectEvent;
-import org.primefaces.model.chart.BarChartModel;
-import org.primefaces.model.chart.ChartSeries;
+//import org.primefaces.model.chart.BarChartModel;
+//import org.primefaces.model.chart.ChartSeries;
 
 @ManagedBean(name = "estadisticaController")
 @SessionScoped
@@ -31,7 +34,7 @@ public class EstadisticaController implements Serializable {
     Institucion estudiante;
     Date fechaInicio;
     Date fechafin;
-    private BarChartModel graficoModelo;
+    private String string_grafico;
     String[] colores = {"008000", "FF0000", "FFD42A", "0000FF", "FFFF00", "00FFFF"};
     List<Color> lista_colores;
 
@@ -43,15 +46,11 @@ public class EstadisticaController implements Serializable {
         lista_colores.add(new Color(0, 255, 255, 0.7));//cyan-convencional
         lista_colores.add(new Color(255, 255, 0, 0.7));//amarillo-emprendedor
         lista_colores.add(new Color(0, 255, 0, 0.7));//verde-realista
-        
+
     }
 
     public void reiniciar() {
 
-    }
-
-    public void setGraficoModelo(BarChartModel graficoModelo) {
-        this.graficoModelo = graficoModelo;
     }
 
     public Institucion getInstitucion() {
@@ -96,49 +95,77 @@ public class EstadisticaController implements Serializable {
 
     public void onDateSelect(SelectEvent event) {
         fechafin = null;
-        graficoModelo = null;
+        string_grafico = null;
         FacesContext facesContext = FacesContext.getCurrentInstance();
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
     }
 
     public void reiniciarEstadistica() {
-        graficoModelo = null;
+        string_grafico = null;
     }
 
-    public BarChartModel getGraficoModelo() {
-        if (grado != null && graficoModelo == null) {
+    public String getGraficoModelo(){
+        System.out.println("getGraficoModelo: "+string_grafico);
+        return string_grafico;
+    }
+    
+    public String cargarGrafico() {
+        if (grado != null && string_grafico == null) {
             System.out.println("grado: " + grado);
             int opcion = detectarTipoEstadistica();
             System.out.println("opcion: " + opcion);
-            List<Datos> listaBarras = null;
+            Datos[] listaBarras = null;
             switch (opcion) {
                 case 11:
                     listaBarras = estadisticaPorGrado();
                     break;
                 default:
-                    graficoModelo = null;
+                    string_grafico = null;
                     FacesContext context = FacesContext.getCurrentInstance();
                     FacesMessage msg;
                     msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "No hay suficientes datos para crear la estadsitica", "");
                     context.addMessage(null, msg);
             }
-            if (listaBarras != null && !listaBarras.isEmpty()) {
-                graficoModelo = new BarChartModel();
-                for (Datos datos : listaBarras) {
-                    ChartSeries barra = new ChartSeries("ambiente:--");
-                    barra.set(datos.tipo, datos.valor);
-                    barra.setLabel(datos.label);
-                    graficoModelo.addSeries(barra);
-                }
-//                graficoModelo.setSeriesColors("");
-                graficoModelo.setShowPointLabels(true);
+            if (listaBarras != null) {
+                string_grafico = obtenerGrafico(listaBarras);
+            }else{
+                string_grafico = null;
             }
-            System.out.println("graficoModelo: " + graficoModelo);
-            return graficoModelo;
+//            System.out.println("graficoModelo: " + string_grafico);
+            return string_grafico;
         }
-        System.out.println("getGraficoModelo derecho");
-        return graficoModelo;
+        System.out.println("getGraficoModelo derecho" + string_grafico);
+        return string_grafico;
+    }
+
+    private String obtenerGrafico(Datos[] listaBarras) {
+        List datas = new ArrayList();
+        datas.add(12.0);
+        datas.add(19.0);
+        datas.add(3.0);
+        datas.add(5.0);
+        datas.add(2.0);
+        datas.add(32.0);
+        List<Color> colors = new ArrayList();
+        colors.add(new Color(255, 0, 0, 0.7));
+        colors.add(new Color(0, 0, 255, 0.7));
+        colors.add(new Color(255, 170, 0, 0.7));
+        colors.add(new Color(0, 255, 255, 0.7));
+        colors.add(new Color(255, 255, 0, 0.7));
+        colors.add(new Color(0, 255, 0, 0.7));
+
+        BarDataset dataset = new BarDataset()
+                .setLabel("sample chart")
+                .setData(datas)
+                .setBackgroundColor(colors)
+                .setBorderWidth(0);
+//        dataset.s
+        BarData data = new BarData();
+        data.setLabels("Rojo", "Azul", "Wednesday", "Thursday", "Friday", "Saturday")
+                .addDataset(dataset);
+        
+        return new BarChart(data).toJson();
     }
 
     private int detectarTipoEstadistica() {
@@ -155,10 +182,16 @@ public class EstadisticaController implements Serializable {
         return opcion;
     }
 
-    public List<Datos> estadisticaPorGrado() {
-        List<Datos> listaBarras = null;
-        if (grado != null && graficoModelo == null) {
-            listaBarras = new ArrayList<>();
+    /**
+     * por ahora trae todos los resultados de las encuestas sin importar si
+     * pretenecen al mismo es
+     *
+     * @return
+     */
+    public Datos[] estadisticaPorGrado() {
+        Datos[] listaBarras = null;
+        List<ResultadoPorAmbiente> listaResultados = new ArrayList();
+        if (grado != null && string_grafico == null) {
             List<EstudianteGrado> listEstudianteGrado = grado.getEstudianteGradoList();
             for (EstudianteGrado estudianteGrado : listEstudianteGrado) {
 
@@ -169,41 +202,67 @@ public class EstadisticaController implements Serializable {
                     if (listaResultadosPorAmbiente.isEmpty()) {
                         continue;
                     }
-                    int i = 0;
-//                  System.out.println("lista:" + listaResultadosPorAmbiente);
-//                    Collections.sort(listaResultadosPorAmbiente, new Comparator<ResultadoPorAmbiente>() {
-//                        @Override
-//                        public int compare(ResultadoPorAmbiente r1, ResultadoPorAmbiente r2) {
-//                            return -r1.getValor().compareTo(r2.getValor());
-//                        }
-//                    });
-                    Datos datos;
-                    for (ResultadoPorAmbiente result : listaResultadosPorAmbiente) {
-                        datos = new Datos();
-                        datos.tipo = result.getTipoAmbiente().getIdTipoAmbiente();
-                        datos.label = result.getTipoAmbiente().getTipo();
-                        datos.valor = (double) result.getValor();
-                        datos.color = colores[datos.tipo - 1];
-//                      System.out.println("result:" + result.getValor());
-                        listaBarras.add(datos);
-                    }
+                    listaResultados.addAll(listaResultadosPorAmbiente);
                 }
             }
-            if (listaBarras.isEmpty()) {
+
+            if (listaResultados.isEmpty()) {
                 FacesContext context = FacesContext.getCurrentInstance();
                 FacesMessage msg;
-                msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "No hay suficientes datos para crear la estadsitica", "");
+                msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "No hay suficientes datos para crear la estadistica", "");
                 context.addMessage(null, msg);
+            } else {
+                listaBarras = promedioResultados(listaResultados);
             }
         }
+        System.out.println("listaBarras: "+listaBarras);
         return listaBarras;
+    }
+
+    public Datos[] promedioResultados(List<ResultadoPorAmbiente> listaResultadosPorAmbiente) {
+        Datos[] listaDatos = new Datos[6];
+        Datos datos;
+        System.out.println("result: " + listaResultadosPorAmbiente);
+        for (ResultadoPorAmbiente result : listaResultadosPorAmbiente) {
+            int tipo = result.getTipoAmbiente().getIdTipoAmbiente();
+            if (listaDatos[tipo - 1] == null) {
+                listaDatos[tipo - 1] = new Datos();
+                listaDatos[tipo - 1].tipo = result.getTipoAmbiente().getIdTipoAmbiente();
+                listaDatos[tipo - 1].label = result.getTipoAmbiente().getTipo();
+                String[] color = result.getTipoAmbiente().getColor().split(",");
+                int r = Integer.parseInt(color[0]);
+                int g = Integer.parseInt(color[1]);
+                int b = Integer.parseInt(color[2]);
+                double a = Double.parseDouble(color[3]);
+                listaDatos[tipo - 1].color = new Color(r, g, b, a);
+            }
+            datos = listaDatos[tipo - 1];
+//            System.out.println("datos: " + datos);
+//            System.out.println("result: " + result);
+            datos.valor += (double) result.getValor();
+        }
+        for (Datos listaDato : listaDatos) {
+            listaDato.valor /= listaResultadosPorAmbiente.size();
+        }
+        return listaDatos;
     }
 
     public class Datos {
 
-        String color;
+        Color color;
         String label;
         int tipo;
         Double valor;
+
+        public Datos() {
+            label = "";
+            valor = 0.0;
+        }
+
+        @Override
+        public String toString() {
+            return "Datos{" + "color=" + color + ", label=" + label + ", tipo=" + tipo + ", valor=" + valor + '}';
+        }
+
     }
 }

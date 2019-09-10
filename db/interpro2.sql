38c31c9c85b330426b8215d80842a137c6aa5db8
anderson <andersonbuitron@unicauca.edu.co>
2019-09-09 20:19:51 -0500

    verificacion y alerta en modo desarrollo + estimado estudiante resultado

--- db/interpro2.sql
+++ db/interpro2.sql
@@ -1,1 +1,20 @@
-sELECT DISTINCT idTipoAmbiente1, idTipoAmbiente2, idTipoAmbiente3, profesion  FROM interpro.datos_riasec x WHERE x.idTipoAmbiente1 = 2 AND x.idTipoAmbiente2 = 5 AND x.idTipoAmbiente3 = 3 
+SELECT DISTINCT 
+	est.idPersona, 
+	est.estrato, 
+	eta.personalidad, 
+	eta.puntajeEncuesta, 
+	eta.puntajeEvaluacion, 
+	per.nombre, 
+	per.apellido, 
+	per.sexo, 
+	est.viveConPadres, 
+	apf.area,
+	rpa.valor
+FROM interpro.Estudiante as est
+INNER JOIN interpro.Persona as per ON per.idPersona = est.idPersona
+INNER JOIN interpro.EstudianteGrado as estg ON estg.idEstudiante = est.idEstudiante
+INNER JOIN interpro.Grado as gd ON estg.idGrado = gd.idGrado
+INNER JOIN interpro.Encuesta as eta ON estg.idEstudiante = eta.idEstudiante AND estg.idGrado = eta.idGrado
+INNER JOIN interpro.AreaProfesional as apf ON apf.idAreaProfesional = eta.idAreaProfesional
+INNER JOIN interpro.ResultadoPorAmbiente as rpa ON rpa.idEncuesta = eta.idEncuesta
+WHERE gd.grado = 8 AND est.idPersona > 100;

a6883df0c78395c8918ce6c278979153c55c85e5
anderson <andersonb@hotmail.es>
2021-03-22 22:30:11 -0500

    calculo para logica de identificacion de estilos de aprendizaje

--- src/main/java/com/ingesoft/interpro/controladores/EstiloController.java
+++ src/main/java/com/ingesoft/interpro/controladores/EstiloController.java
@@ -44,32 +44,71 @@ public class EstiloController  extends Controller implements Serializable {
     protected void initializeEmbeddableKey() {
     }
 
-    public String getCalculo(){
+    public void getCalculo(){
+        
         int idEncuesta = 1;
         Encuesta encuesta = this.getEncuestaController().getEncuesta(idEncuesta);
+        ContadorTiposEstilos[] listaContadorTiposEstilos = this.getCalculoTiposEstilos(encuesta);
+        for (ContadorTiposEstilos item : listaContadorTiposEstilos) {
+            System.out.println("resta: "+ item.contador + "| tipoEstilo: "+item.tipoEstilo.getNombre());
+            
+        }
+        
+    }
+    
+    /**
+     * Calcula Estadistica de tipo estilo de una encuesta
+     * @param encuesta
+     * @return 
+     */
+    private ContadorTiposEstilos[] getCalculoTiposEstilos(Encuesta encuesta){
         this.items = this.getRespuestaEstiloController().getItemsXEncuesta(encuesta);
         
-        ContadorTiposEstilos[] contador = new ContadorTiposEstilos[8]; /** 8 es la cantidad de tipos de estilo */
+        if(this.items == null ){
+            return null;
+        }
+        ContadorTiposEstilos[][] contador = new ContadorTiposEstilos[2][4]; /** 8 es la cantidad de tipos de estilo */
+        contador[0] = new ContadorTiposEstilos[4];
+        contador[1] = new ContadorTiposEstilos[4];
+        int indice;
+        int columna;
+        int fila;
         
+        /** Sumatoria de tipos de estilo de las respuestas */
         for (RespuestaEstilo item : this.items) {
             PreguntaEstilosAprendizajeFs pregunta = item.getIdpreguntaEstilos();
             List<TipoEstiloPregunta> listaTiposEstiloPregunta = pregunta.getTipoestiloPreguntaList();
             TipoEstiloPregunta obj = (listaTiposEstiloPregunta.get(0).getIndice().equals(item.getRespuesta()))?listaTiposEstiloPregunta.get(0):listaTiposEstiloPregunta.get(1);
-            System.out.println("enunciado: "+ (pregunta.getEnunciado()));
                  
-            int indice = obj.getTipoEstilo().getIdTipoEstilo()-1;
-            if(contador[indice] == null) {
-                contador[indice] = new ContadorTiposEstilos();
+            indice = obj.getTipoEstilo().getIdTipoEstilo()-1;
+            columna = indice % 2;
+            fila = indice / 2;
+            if(contador[columna][fila] == null) {
+                contador[columna][fila] = new ContadorTiposEstilos();
             }
-            contador[indice].contador++;
-            contador[indice].tipoEstilo = obj.getTipoEstilo();
+            contador[columna][fila].contador++;
+            contador[columna][fila].tipoEstilo = obj.getTipoEstilo();
             
         }
-        for (ContadorTiposEstilos contadorTiposEstilos : contador) {
+        ContadorTiposEstilos[] vectorRes = new ContadorTiposEstilos[4];
+        int resta;
+        /** Calculo de grupos de tipos de estilo */
+        for (int i = 0; i < 4; i++) {
             
-            System.out.println("sum: "+ (contadorTiposEstilos.contador) + "| tipoEstilo: "+contadorTiposEstilos.tipoEstilo.getNombre());
+            indice = contador[0][i].contador > contador[1][i].contador ? 0 : 1;
+            resta = Math.abs(contador[0][i].contador - contador[1][i].contador);
+            
+            if(vectorRes[i] == null) {
+                vectorRes[i] = new ContadorTiposEstilos();
+            }
+            vectorRes[i].tipoEstilo = contador[indice][i].tipoEstilo;
+            vectorRes[i].contador = resta;
+            
+//            System.out.println("sum: "+ (contador[0][i].contador) + "| tipoEstilo: "+contador[0][i].tipoEstilo.getNombre());
+//            System.out.println("sum: "+ (contador[1][i].contador) + "| tipoEstilo: "+contador[1][i].tipoEstilo.getNombre());
+            
         }
-        return "";
+        return vectorRes;
     }
     
     class ContadorTiposEstilos {
